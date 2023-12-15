f42ac2f5f3c544f63f6e5bbdf8baaf2457deaefe
Ridham24 <ridhamgoel03@gmail.com>
2023-12-15 16:44:04 +0530

    Recommendations

--- src/Components/VideoDetails.jsx
+++ src/Components/VideoDetails.jsx
@@ -5,17 +5,23 @@ import { Box, Stack, Typography } from '@mui/material'
 import { FetchApi } from '../FetchApi'
 import ReactPlayer from 'react-player'
 import { CheckCircle } from "@mui/icons-material"
+import Videos from './Videos'
 
 const VideoDetails = () => {
   const { id } = useParams()
+  const [Load,setLoad]=useState(true)
   const [videoDetails, setVideodetails] = useState(null)
+  const [related, setRelated] = useState(null)
   useEffect(() => {
-    FetchApi(`videos?part=snippet,statistics&id=${id}`).then((data) =>setVideodetails(data.items[0]))
+    FetchApi(`videos?part=snippet,statistics&id=${id}`).then((data) => setVideodetails(data.items[0]))
+    setLoad(true)
+    FetchApi(`search?part=snippet&relatedToVideoId=${id}&type=video`).then((data) => setRelated(data.items))
+    setLoad(false)
   }, [id])
   console.log(videoDetails);
   return (
     <Box minHeight="95vh">
-      <Stack direction={{ md: 'row', xs: 'coloumn' }}>
+      <Stack direction={{ md: 'row', xs: 'column' }}>
         <Box flex={1}>
           <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
             <ReactPlayer
@@ -32,9 +38,7 @@ const VideoDetails = () => {
               color="#fff"
               px={2}
             >
-              <Link
-                to={`/channel/${videoDetails?.snippet?.channelId}`}
-              >
+              <Link to={`/channel/${videoDetails?.snippet?.channelId}`}>
                 <Typography variant="subtitle1" color="#fff">
                   {videoDetails?.snippet?.channelTitle}
                   <CheckCircle
@@ -42,7 +46,7 @@ const VideoDetails = () => {
                   />
                 </Typography>
               </Link>
-              <Stack direction='row' gap={2} alignItems='center'>
+              <Stack direction="row" gap={2} alignItems="center">
                 <Typography variant="body1">
                   {parseInt(
                     videoDetails?.statistics?.viewCount
@@ -58,6 +62,14 @@ const VideoDetails = () => {
               </Stack>
             </Stack>
           </Box>
+        </Box>
+        <Box
+          px={1}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={related} Load={Load} direction="column" />
         </Box>
       </Stack>
     </Box>
--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -3,18 +3,18 @@ import VideoCard from './VideoCard'
 import ChannelCard from './ChannelCard'
 import Loader from './Loader'
 import React from 'react'
-const Videos = ({ videos, Load ,flag}) => {
+const Videos = ({ videos, Load ,flag,direction}) => {
   console.log(videos)
   if (Load) return <Loader />
   return (
     <div>
       <Stack
         flexWrap="wrap"
-        direction="row"
+        direction={direction||"row"}
         gap={2}
         justifyContent="start"
       >
-        {videos.map((video, ind) => (
+        {videos?.map((video, ind) => (
           <Box key={ind}>
             {!video.id.channelId && <VideoCard videoDetail={video} />}
             {!flag&&video.id.channelId && <ChannelCard channelDetail={video} />}
